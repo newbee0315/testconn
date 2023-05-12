87991913725e6b9f081dc4252cc43b1c8a011318
wulala <46306872+newbee0315@users.noreply.github.com>
2023-05-12 11:13:11 +0800

    Update Hello.py

--- Hello.py
+++ Hello.py
@@ -1,11 +1,1 @@
-# streamlit_app.py
-
-import streamlit as st
-
-# Initialize connection.
-conn = st.experimental_connection('mysql', type='sql')
-
-# Perform query.
-df = conn.query('SELECT * from a;', ttl=600)
-
-df
+"你好"
